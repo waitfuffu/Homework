f5166215e6f47531b440a950b841c32fca2a0380
waitfuffu <waitfuffu>
2022-04-29 13:59:13 +0800

    add homework 2 replace

--- app/src/main/java/com/example/app04/choose2/DetailFragment.java
+++ app/src/main/java/com/example/app04/choose2/DetailFragment.java
@@ -1,5 +1,4 @@
 package com.example.app04.choose2;
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +6,8 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,11 +17,71 @@ public class DetailFragment extends Fragment {
     private TextView textView ;
     private SeekBar seekBar ;
     private LinearLayout layout;
+    private int position;
+
+    public int getPosition() {
+        return position;
+    }
+
+    public DetailFragment(int position) {
+        this.position = position ;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        init();
+        init(this.position);
+        return this.layout;
+
+    }
+
+    //页面初始化
+    public void init(int position){
+        switch (position) {
+            case 0:
+                doZeroTODO();
+                break;
+            case 1:
+                doOneTODO();
+                break;
+            default:
+                doOtherTODO();
+                break;
+        }
+
+    }
+
+    private void doOtherTODO() {
+
+        layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+        textView = new TextView(getActivity());
+        textView.setText("未完成功能");
+        textView.setTextSize(50);
+        layout.addView(textView);
+    }
+
+    private void doOneTODO() {
+        layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+        textView = new TextView(getActivity());
+        textView.setText("音量为 ：30"); //模拟音量
+        textView.setTextSize(50);
+        layout.addView(textView);
+
+    }
+
+    private void doZeroTODO() {
+        layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+        seekBar = new SeekBar(getActivity());
+        textView = new TextView(getActivity());
+        //系统最大屏幕亮度值设为seekbar的最大进度值
+        seekBar.setMax(BrightnessUtil.getMaxBrightness(getActivity()));
+        //系统当前屏幕亮度值设为seekbar当前进度值
+        seekBar.setProgress(BrightnessUtil.getBrightness(getActivity()));
+        //设置当前屏幕亮度显示
+        textView.setText("值为 ："+BrightnessUtil.getBrightness(getActivity()));
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -43,20 +104,6 @@ public class DetailFragment extends Fragment {
         });
         layout.addView(seekBar);
         layout.addView(textView);
-        return layout;
-    }
 
-    //页面初始化
-    public void init(){
-        layout = new LinearLayout(getActivity());
-        layout.setOrientation(LinearLayout.VERTICAL);
-        seekBar = new SeekBar(getActivity());
-        textView = new TextView(getActivity());
-        //系统最大屏幕亮度值设为seekbar的最大进度值
-        seekBar.setMax(BrightnessUtil.getMaxBrightness(getActivity()));
-        //系统当前屏幕亮度值设为seekbar当前进度值
-        seekBar.setProgress(BrightnessUtil.getBrightness(getActivity()));
-        //设置当前屏幕亮度显示
-        textView.setText("值为 ："+BrightnessUtil.getBrightness(getActivity()));
     }
 }
--- app/src/main/java/com/example/app04/choose2/ListFragment.java
+++ app/src/main/java/com/example/app04/choose2/ListFragment.java
@@ -4,30 +4,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
 import com.example.app04.R;
 
 //左边ListFragment
 public class ListFragment extends androidx.fragment.app.ListFragment {
     //默认为横屏 （ 两个fragment）
-String[] title = new String[]{"亮度","声音","....."};//设置选项
+    String[] title = new String[]{"亮度", "声音", "....."};//设置选项
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,title));
+        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, title));
 
     }
 
     @Override
     public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        if (position==0){   //点击了第一个选项
-            DetailFragment detailFragment = new DetailFragment();
+        DetailFragment details = (DetailFragment)getFragmentManager()
+                .findFragmentById(R.id.frameLay);
+        if (details == null || details.getPosition()!=position){
+            details  =  new DetailFragment(position);
             FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.add(R.id.frameLay,detailFragment).commit();
+            ft.replace(R.id.frameLay,details);//内容替换
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);//转换特效
+            ft.commit();
         }
     }
 }
